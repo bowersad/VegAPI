f3e9a1f13c9dd1087a3b932c6a2c0544619e7cb0
bowersad <bowersad@gmail.com>
2016-06-21 13:25:06 -0400

    Moving back to Express.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
-//var express = require('express');
-var http = require('http');
+var express = require('express');
 
-/*app = express();	
-app.set('port',5000);
-app.listen(app.get('port'), function () {
-	console.log('listening on port %d', app.get('port'));
-});*/
 
-function handleRequest(request, response) {
-		response.end('It works!! Path Hit:' + request.url);
-}
-
-const SERVERPORT = 8080;
-var server = http.createServer(handleRequest);
-server.listen(SERVERPORT, function() {
-	console.log("Server listening on: http://localhost:%s", SERVERPORT);
+app = express();	
+app.get('/',function (req, res) {
+	res.send('Hello World');
 })
+app.listen(8080, function () {
+	console.log('listening on port %d', 8080);
+});
+
