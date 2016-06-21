7e0fa67c9fadb06aabb49313c1b8859dfb1d5bc2
bowersad <bowersad@gmail.com>
2016-06-21 11:14:16 -0400

    Trying http instead of express

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-var express = require('express');
+//var express = require('express');
+var http = require('http');
 
-app = express();
-	
+/*app = express();	
 app.set('port',5000);
-
 app.listen(app.get('port'), function () {
-	console.log('listening on port ' + app.get('port'));
+	console.log('listening on port %d', app.get('port'));
+});*/
+
+function handleRequest(request, response) {
+		response.end('It works!! Path Hit:' + request.url);
+}
+
+const SERVERPORT = 8080;
+var server = http.createServer(handleRequest);
+server.listen(SERVERPORT, function() {
+	console.log("Server listening on: http://localhost:%s", SERVERPORT);
 })
