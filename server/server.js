c09df0406b6421f93491c69731843c1e3cad2e5c
bowersad <bowersad@gmail.com>
2016-06-23 09:18:32 -0400

    Playing with form submission

--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ var express = require('express');
 
 
 app = express();	
-app.get('/',function (req, res) {
-	res.send('Hello World');
+app.get('/index.htm',function (req, res) {
+	res.sendFile(__dirname + "/index.htm");
 });
 
-app.put('/puthere', function (req, res) {
-	res.send('you put here');
+app.get('/process', function (req, res) {
+	response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
 });
 
-app.listen(8080, function () {
+app.listen(8080, function () {	
+	console.log(__dirname);
 	console.log('listening on port %d', 8080);
 });
 
