a70a3ccd4abeacaa5669a16490b29e9c9148fa6f
bowersad <bowersad@gmail.com>
2016-06-23 08:44:22 -0400

    Playing with routes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ var express = require('express');
 app = express();	
 app.get('/',function (req, res) {
 	res.send('Hello World');
-})
+});
+
+app.put('/puthere', function (req, res) {
+	res.send('you put here');
+});
+
 app.listen(8080, function () {
 	console.log('listening on port %d', 8080);
 });
