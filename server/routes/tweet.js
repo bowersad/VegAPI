9215a2203c7312baf8b84ff44b2a4bf4a8468489
bowersad <bowersad@gmail.com>
2016-08-04 13:25:12 -0400

    Fixed res, req bug

--- server/routes/tweet.js
+++ server/routes/tweet.js
@@ -1,6 +1,6 @@
 var tokenSecret;
 
-exports.GetToken = function (res, req) {
+exports.GetToken = function (req, res) {
 	var twitterAPI = require('node-twitter-api');
 	var twitter = new twitterAPI({
 		consumerKey: 'GJx8RleUiEh2CPgvS1bKIC952',
@@ -11,7 +11,7 @@ exports.GetToken = function (res, req) {
 
 	twitter.getRequestToken(function (error, requestToken, requestTokenSecret, results) {
 		if (error) {
-			console.log("Error getting OAuth request toke : " + error);
+			console.log("Error getting OAuth request token : " + error);
 		} else	{
 			tokenSecret = requestTokenSecret;
 		}
