fc99717f0d5357346e7ed655c31f73b76fb92bf4
bowersad <bowersad@gmail.com>
2016-08-24 10:02:18 -0400

    Fixed token issue

--- server/routes/tweet.js
+++ server/routes/tweet.js
@@ -7,13 +7,15 @@ exports.GetToken = function (req, res) {
 		consumerSecret: 'WyIw3BQsGr3zg9tY5AzDMdKACMnBC7VcgZdnqnO5Ot0HcDveUJ',
 		callback: ''
 	});
-	var tokenSecret;
+	var accessToken;
+	var accessTokenSecret;
 
 	twitter.getRequestToken(function (error, requestToken, requestTokenSecret, results) {
 		if (error) {
 			console.log("Error getting OAuth request token : " + error);
 		} else	{
-			tokenSecret = requestTokenSecret;
+			accessToken = requestToken;
+			accessTokenSecret = requestTokenSecret;
 		}
 	});
 
